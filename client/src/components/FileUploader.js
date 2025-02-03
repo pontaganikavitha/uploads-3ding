1173cf63e78c80c1eaa5a6542a7d7397e1499600
pontaganikavitha <kavitha3ding.in>
2025-02-03 12:38:23 +0530

    Initial commit

--- client/src/components/FileUploader.js
+++ client/src/components/FileUploader.js
@@ -39,7 +39,7 @@ const FileUploader = ({ session, orderId, onComplete }) => {
         formData.append('orderId', orderId);
 
         try {
-          await fetch('http://54.66.86.34:3001/upload', {
+          await fetch('http://13.55.42.43:3001/upload', {
             method: 'POST',
             body: formData,
           });
